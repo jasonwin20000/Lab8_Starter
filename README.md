1446c098d3ac7905506a3f396fd601db14df5626
Jason Win <jasonwin2000@gmail.com>
2021-11-13 14:20:58 -0800

    finished

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Lab 8 - Starter
+# Lab 8 - Jason Win
+
+# Check your understanding questions: 
+1) I would fit my automated tests in my recipe project development pipeline within a Github action that runs whenever code is pushed because we want to conisistently be able to test our code whenever it gets pushed so that bugs get found as things are being developed and not have everything piled up at the end.
+2) No, I wouldn't use an end to end test to check if a function is returning the correct output.
+3) No, I wouldn't use a unit test to test the "message" feature because the message feature would involve many other components and functions that would also need to be tested that a unit test would not be able to encapsulate. 
+4) Yes, I would use a unit test to test the "max message length" feature as that is a indivdual part of the code that can be tested with a unit test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,68 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('phone number #1', () => {
+    expect(functions.isPhoneNumber('408-643-5550')).toBe(true);
+});
+test('phone number #2', () => {
+    expect(functions.isPhoneNumber('669-123-4567')).toBe(true);
+});
+test('fake phone number', () => {
+    expect(functions.isPhoneNumber('21212121212122122')).toBe(false);
+});
+test('an email is not a phone number', () => {
+    expect(functions.isPhoneNumber('jasonwin2000@gmail.com')).toBe(false);
+});
+
+test('my email', () => {
+    expect(functions.isEmail('jasonwin2000@gmail.com')).toBe(true);
+});
+test('my email #2', () => {
+    expect(functions.isEmail('jwin@ucsd.edu')).toBe(true);
+});
+test('fake email', () => {
+    expect(functions.isEmail('slahdaslsadkjh@whoopdy')).toBe(false);
+});
+test('a phone number is not an email', () => {
+    expect(functions.isEmail('408-643-5550')).toBe(false);
+});
+
+test('strong password #1', () => {
+    expect(functions.isStrongPassword('VeryStrong21')).toBe(true);
+});
+test('strong password #2', () => {
+    expect(functions.isStrongPassword('SuperDuper_PW')).toBe(true);
+});
+test('not strong password #1', () => {
+    expect(functions.isStrongPassword('VeryStrong21asdhksdajfhglkadjsflkjaddsafasdfasdsad')).toBe(false);
+});
+test('not strong password #2', () => {
+    expect(functions.isStrongPassword('sdjfkdjs@a;.')).toBe(false);
+});
+
+test('date #1', () => {
+    expect(functions.isDate('12/23/2000')).toBe(true);
+});
+test('date #2', () => {
+    expect(functions.isDate('1/3/2021')).toBe(true);
+});
+test('bad date #1', () => {
+    expect(functions.isDate('12/23/00')).toBe(false);
+});
+test('date #2', () => {
+    expect(functions.isDate('12-23-00')).toBe(false);
+});
+
+test('color #1', () => {
+    expect(functions.isHexColor('FFF')).toBe(true);
+});
+test('color #2', () => {
+    expect(functions.isHexColor('ABC')).toBe(true);
+});
+test('fake color #1', () => {
+    expect(functions.isHexColor('A')).toBe(false);
+});
+test('fake color #2', () => {
+    expect(functions.isHexColor('aslkdjlkasjd')).toBe(false);
+});
+
